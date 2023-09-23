e44be695d31f07fb5ab36ccc4e93625b59225e84
cibulka <cibulka.me@gmail.com>
2023-09-23 03:49:26 +0200

    fix: removed manifest

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -25,7 +25,6 @@ export default function LocaleLayout(
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color={THEME_DARK.action} />
         <meta name="msapplication-TileColor" content={THEME_DARK.action} />
         <meta name="theme-color" content="#ffffff" />
