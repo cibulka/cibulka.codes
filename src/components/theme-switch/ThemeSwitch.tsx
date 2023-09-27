118ec3399e1257e9a1a641c820bdb542e16695b8
cibulka <cibulka.me@gmail.com>
2023-09-28 01:59:06 +0200

    fix: dark mode switch as event, not class

--- src/components/theme-switch/ThemeSwitch.tsx
+++ src/components/theme-switch/ThemeSwitch.tsx
@@ -5,20 +5,22 @@ import { ACTION } from '@/context/App.actionTypes';
 import { useAppContext } from '@/context/App.utils';
 import { IconMoon } from '@/icons/IconMoon';
 import { IconSun } from '@/icons/IconSun';
-import { getClassNamePlausible } from '@/utils/plausible';
+import { usePlausibleEvent } from '@/utils/plausible';
 
 export function ThemeSwitch(props: { labels: { dark: string; light: string } }) {
   const { dispatch, state } = useAppContext();
   const isDarkMode = state.theme.selected === THEME_MODE.DARK;
+  const onClickPlausible = usePlausibleEvent(PLAUSIBLE_GOALS.DARK_MODE);
   return (
     <button
-      className={['w-6 h-6', getClassNamePlausible(PLAUSIBLE_GOALS.DARK_MODE)].join(' ')}
+      className="w-6 h-6"
       type="button"
       onClick={() => {
         dispatch({
           type: ACTION.THEME_MODE,
           payload: isDarkMode ? THEME_MODE.LIGHT : THEME_MODE.DARK,
         });
+        if (onClickPlausible) onClickPlausible();
       }}
       aria-label={isDarkMode ? props.labels.light : props.labels.dark}
     >
