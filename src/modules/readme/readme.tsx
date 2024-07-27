e5e7d7aa2952683016f885f5e519134a957c42a6
Petr Cibulka <cibulka.me@gmail.com>
2024-07-27 22:33:19 +0200

    fix: fix typo in casing

--- src/modules/readme/readme.tsx
+++ src/modules/readme/readme.tsx
@@ -4,7 +4,8 @@ import { Paper } from '@/shared/components/paper';
 import { MdxReader } from '@/shared/mdx-reader';
 import { getDocuments } from '@/shared/mdx-reader/get-documents';
 import { PropsWithLocale } from '@/types/params';
-import styles from './Readme.module.css';
+
+import styles from './readme.module.css';
 
 export function Readme(props: PropsWithLocale) {
   const readme = getDocuments(['Readme'], props.locale).find((doc) => doc.slug === 'readme');
