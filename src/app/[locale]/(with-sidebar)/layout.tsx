815bf01fadc980598eb047875e003c0af9a11139
Petr Cibulka <cibulka.me@gmail.com>
2024-07-27 22:29:39 +0200

    fix: remove console.log

--- src/app/[locale]/(with-sidebar)/layout.tsx
+++ src/app/[locale]/(with-sidebar)/layout.tsx
@@ -12,7 +12,6 @@ export default async function LayoutWithSidebar({
   modal,
   params: { locale },
 }: PropsWithChildren & ParamsWithLocale & { modal: ReactNode }) {
-  console.log({ modal });
   return (
     <>
       <div className="relative">
