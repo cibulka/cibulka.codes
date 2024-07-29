4a28f5fe6af7f8fc16dd30ae450d3a64fc457c71
Petr Cibulka <cibulka.me@gmail.com>
2024-07-29 14:44:40 +0200

    fix: use date object in dayjs diff function for mobile devices

--- src/shared/blocks/years-of-experience/years-of-experience.tsx
+++ src/shared/blocks/years-of-experience/years-of-experience.tsx
@@ -7,7 +7,7 @@ import { EXPERIENCE_START } from '@/constants/config';
 
 import { yearMessage } from './messages';
 
-const years = dayjs(new Date()).diff(EXPERIENCE_START, 'years');
+const years = dayjs(new Date()).diff(new Date(EXPERIENCE_START), 'years');
 
 export function YearsOfExperience() {
   const { formatMessage } = useIntl();
