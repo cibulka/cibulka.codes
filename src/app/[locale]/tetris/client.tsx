b212f8e18c7f8369b067134ef9abd6f21505ef56
Petr Cibulka <cibulka.me@gmail.com>
2024-08-08 00:10:09 +0200

    chore: update tetris

--- src/app/[locale]/tetris/client.tsx
+++ src/app/[locale]/tetris/client.tsx
@@ -2,9 +2,5 @@
 import Tetris from 'react-tetris-ts';
 
 export function TetrisClient() {
-  return (
-    <div className="relative w-screen h-screen">
-      <Tetris />
-    </div>
-  );
+  return <Tetris />;
 }
